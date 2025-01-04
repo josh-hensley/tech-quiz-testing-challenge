14aba79e9e9cfb007ee1839ccd641358d6b0b78a
josh-hensley <me@joshhensley.com>
2025-01-03 21:56:16 -0600

    e2e testing is complete

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,15 +1,43 @@
 describe('Quiz Cycle', ()=>{
+    beforeEach(()=>{
+        cy.visit('http://localhost:3001')
+    })
     context('Quiz Setup', ()=>{
-        it('should render', () => {
-            cy.visit('http://localhost:3001')
+        it('should render', () => {});
+        it('should start quiz when button is clicked', ()=>{
+            cy.get('button').contains('Start Quiz').click();
+            cy.get('h2').should('exist');
+            cy.get('.btn').eq(0).contains(1);
+        });
+    });
+    context('Quiz in progress', ()=>{
+        beforeEach(()=>{
+            cy.get('button').click();
+        })
+        it('should present a new question when answer is clicked', ()=>{
+            cy.get('h2').then(($el)=>{
+                cy.get('.btn').eq(0).click()
+                cy.get('h2').should('not.equal', $el)
+            });  
+        });
+        it('should end the quiz when all questions are answered', ()=>{
+            for (let i = 0; i < 10; i++) {
+                cy.get('.btn').eq(0).click();
+            }
+            cy.get('h2').contains('Quiz Completed')
         });
     });
 
-    context('Quiz in progress', ()=>{
-
-    });
-
     context('Quiz Restart', ()=>{
-
-    })
-})
+        it('Should restart with new questions when Take New Quiz is clicked', ()=>{
+            cy.get('button').click();
+            cy.get('h2').then(($el)=>{
+                for (let i = 0; i < 10; i++){
+                    cy.get('.btn').eq(0).click()
+                }
+                cy.get('.btn').contains('Take New Quiz').click();
+                cy.get('h2').should('not.equal', $el);
+            })
+        })
+    });
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,6 @@ import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 await db();
 
 const PORT = process.env.PORT || 3001;
@@ -20,10 +14,10 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
    app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, './client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
 
